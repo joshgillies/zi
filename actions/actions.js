f9e108fb1efa891b4de2b1b15b69ee6778bcc836
Josh Gillies <github@joshgilli.es>
2015-02-15 21:53:11 +1100

    Update actions

---
+++ actions/actions.js
@@ -0,0 +1,109 @@
+var helpers = require('./helpers');
+var actionId = helpers.actionId;
+var useKey = helpers.useKey;
+
+var ATTRIBUTES = {
+  name: actionId('set_{0}_name'),
+  short_name: actionId('set_{0}_short_name'),
+  not_found_page_cache_globally: actionId('set_{0}_not_found_page_cache_globally'),
+  attributes: actionId('set_{0}_attributes')
+};
+
+var ACTIONS = {
+  add_web_path: actionId('add_{0}_path'),
+  create_asset: actionId('create_{0}'),
+  create_link: actionId('link_notice_{0}_to_{1}', 2),
+  set_attribute_value: ATTRIBUTES
+};
+
+var LINKS = {
+  TYPE_1: 1,
+  TYPE_2: 2,
+  TYPE_3: 4,
+  NOTICE: 8
+};
+
+function getActionId(type) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var action = ACTIONS[useKey(type)];
+  if (typeof action === 'object')
+    action = action[useKey(args.shift())];
+  return action.apply(null, args);
+}
+
+function actionAddPath(opts) {
+  var type = 'add_web_path';
+
+  return {
+    action_id: getActionId.apply(null, [type, opts.id]),
+    action_type: type,
+    asset: opts.assetId, // id of asset performing action against
+    path: opts.path // web path
+  };
+}
+
+function actionCreateAsset(opts) {
+  var type = 'create_asset';
+
+  return {
+    action_id: getActionId.apply(null, [type, opts.id]),
+    action_type: type,
+    type_code: opts.type,
+    parentid: opts.parentId || 1,
+    value: opts.value || '',
+    link_type: opts.link || LINKS['TYPE_1'],
+    is_dependant: opts.dependant || 0,
+    is_exclusive: opts.exclusive || 0
+  };
+}
+
+function actionCreateLink(opts) {
+  var type = 'create_link';
+
+  return {
+    action_id: getActionId.apply(null, [type, opts.to, opts.from]),
+    action_type: type,
+    asset: opts.assetId,
+    value: opts.value || '',
+    link_type: opts.link || LINKS['TYPE_1'],
+    is_dependant: opts.dependant || 0,
+    is_exclusive: opts.exclusive || 0,
+    assetid: '', // id of asset linking to
+    is_major: '' // ???
+  };
+}
+
+function actionSetAttribute(opts) {
+  var type = 'set_attribute_value';
+
+  return {
+    action_id: getActionId.apply(null, [type, opts.attribute, opts.id]),
+    action_type: type,
+    asset: opts.assetId,
+    attribute: opts.attribute,
+    value: opts.value || ''
+  };
+}
+
+exports.createAction = function createAction(type, opts) {
+  type = useKey(type);
+
+  if (type === 'add_web_path') {
+    return actionAddPath(opts);
+  }
+
+  if (type === 'create_asset') {
+    return actionCreateAsset(opts);
+  }
+
+  if (type === 'set_attribute_value') {
+    return actionSetAttribute(opts);
+  }
+
+  if (type === 'create_link') {
+    return actionCreateLink(opts);
+  }
+
+  throw new Error('Unknown action type of \'' + type + '\'');
+};
+exports.getActionId = getActionId;
---
+++ actions/helpers.js
@@ -0,0 +1,26 @@
+var format = require('string-template');
+
+var HELPERS = {
+  cache_globally: 'not_found_page_cache_globally',
+  set_attribute: 'set_attribute_value',
+  add_path: 'add_web_path'
+};
+
+exports.useKey = function useKey(key) {
+  return HELPERS[key] || key;
+};
+
+exports.asset = function asset(type, id) {
+  return format('{0}_{1}', type, id);
+};
+
+exports.actionId = function actionId(template, expected) {
+  return function formatter() {
+    var args = Array.prototype.slice.call(arguments);
+    expected = expected || 1;
+    if (args.length === expected)
+      return format(template, args);
+    throw new Error(format('Expected {0} arguments instead got {1}', expected, args.length));
+  };
+};
+
