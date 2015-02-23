5ddf603b54c1bac68f60b5a5ecd2481136fe1552
Josh Gillies <github@joshgilli.es>
2015-02-23 13:03:46 +1100

    Update tests

--- test/test-actions.js
+++ test/test-actions.js
@@ -3,8 +3,8 @@ var actions = require('../actions');
 var test = require('tape');
 
 test('use shorthand helpers', function(t) {
-  t.equal(helpers.useKey('add_path'), 'add_web_path');
-  t.equal(helpers.useKey('create_asset'), 'create_asset');
+  t.equal(helpers.useKey('add_path'), 'add_web_path', 'shorthand selector');
+  t.equal(helpers.useKey('create_asset'), 'create_asset', 'passthrough when no shortahnd is available');
   t.end();
 });
 
@@ -16,19 +16,23 @@ test('join asset type with id', function(t) {
 test('create action ids', function(t) {
   t.equal(
     actions.getActionId('add_path', 'Site_1'),
-    'add_Site_1_path'
+    'add_Site_1_path',
+    'add path action id'
   );
   t.equal(
     actions.getActionId('create_link', 1, 2),
-    'link_notice_1_to_2'
+    'link_notice_1_to_2',
+    'create link action id'
   );
   t.equal(
     actions.getActionId('set_attribute', 'name', 'Site_1'),
-    'set_Site_1_name'
+    'set_Site_1_name',
+    'set attribute action id'
   );
   t.equal(
-    actions.getActionId('set_attribute', 'cache_globally', 'Site_1'),
-    'set_Site_1_not_found_page_cache_globally'
+    actions.getActionId('set_permission', 1, 1, 2),
+    'set_permission_1_1_2',
+    'set permission action id'
   );
   t.end();
 });
