8392963a2765c291912c18540845c3374202be95
Josh Gillies <github@joshgilli.es>
2015-02-26 22:30:27 +1100

    Update action tests

--- test/test-actions.js
+++ test/test-actions.js
@@ -38,34 +38,112 @@ test('create action ids', function(t) {
 });
 
 test('create actions', function(t) {
-  var action = actions.createAction('create_asset', {
-    id: 'Site_1',
-    parentId: 1,
-    type: 'site'
-  });
-  var expected = {
-    action_id: 'create_Site_1',
-    action_type: 'create_asset',
-    type_code: 'site',
-    parentid: 1,
-    value: '',
-    link_type: 1,
-    is_dependant: 0,
-    is_exclusive: 0
+  var tests = {
+    add_web_path: {
+      action: actions.createAction('add_web_path', {
+        id: 'Site_1',
+        path: 'test-site',
+        assetId: '1'
+      }),
+      expected: {
+        action_id: 'add_Site_1_path',
+        action_type: 'add_web_path',
+        asset: '1',
+        path: 'test-site'
+      },
+      xml: [
+        '<action>',
+        '    <action_id>add_Site_1_path</action_id>',
+        '    <action_type>add_web_path</action_type>',
+        '    <asset>1</asset>',
+        '    <path>test-site</path>',
+        '</action>'
+      ].join('\n')
+    },
+    create_asset: {
+      action: actions.createAction('create_asset', {
+        id: 'Site_1',
+        parentId: 1,
+        type: 'site'
+      }),
+      expected: {
+        action_id: 'create_Site_1',
+        action_type: 'create_asset',
+        type_code: 'site',
+        parentid: 1,
+        value: '',
+        link_type: 1,
+        is_dependant: 0,
+        is_exclusive: 0
+      },
+      xml: [
+        '<action>',
+        '    <action_id>create_Site_1</action_id>',
+        '    <action_type>create_asset</action_type>',
+        '    <type_code>site</type_code>',
+        '    <parentid>1</parentid>',
+        '    <value></value>',
+        '    <link_type>1</link_type>',
+        '    <is_dependant>0</is_dependant>',
+        '    <is_exclusive>0</is_exclusive>',
+        '</action>'
+      ].join('\n')
+    },
+    create_link: {
+      action: actions.createAction('set_attribute_value', {
+        id: 'Site_1',
+        assetId: 1,
+        attribute: 'html',
+        value: 'Test Site'
+      }),
+      expected: {
+        action_id: 'set_html_Site_1',
+        action_type: 'set_attribute_value',
+        asset: 1,
+        attribute: 'html',
+        value: 'Test Site'
+      },
+      xml: [
+        '<action>',
+        '    <action_id>set_html_Site_1</action_id>',
+        '    <action_type>set_attribute_value</action_type>',
+        '    <asset>1</asset>',
+        '    <attribute>html</attribute>',
+        '    <value>Test Site</value>',
+        '</action>'
+      ].join('\n')
+    },
+    set_permission: {
+      action: actions.createAction('set_permission', {
+        assetId: 1,
+        permission: 'read',
+        granted: true,
+        userId: 7
+      }),
+      expected: {
+        action_id: 'set_permission_1_read_7',
+        action_type: 'set_permission',
+        asset: 1,
+        permission: 1,
+        granted: 1,
+        userid: 7
+      },
+      xml: [
+        '<action>',
+        '    <action_id>set_permission_1_read_7</action_id>',
+        '    <action_type>set_permission</action_type>',
+        '    <asset>1</asset>',
+        '    <permission>1</permission>',
+        '    <granted>1</granted>',
+        '    <userid>7</userid>',
+        '</action>'
+      ].join('\n')
+    }
   };
-  var xml = [
-    '<action>',
-    '    <action_id>create_Site_1</action_id>',
-    '    <action_type>create_asset</action_type>',
-    '    <type_code>site</type_code>',
-    '    <parentid>1</parentid>',
-    '    <value></value>',
-    '    <link_type>1</link_type>',
-    '    <is_dependant>0</is_dependant>',
-    '    <is_exclusive>0</is_exclusive>',
-    '</action>'
-  ].join('\n');
-  t.deepEqual(action, expected, 'create asset object');
-  t.equal(action.toXML(), xml, 'create asset XML');
+  Object.keys(tests).forEach(function(test) {
+    var action = tests[test].action;
+    t.deepEqual(action, tests[test].expected, 'action ' + test + ' object');
+    t.equal(action.toXML(), tests[test].xml, 'action ' + test + ' XML');
+  });
   t.end();
 });
